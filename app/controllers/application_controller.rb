5d08de42989360d24abeb124e56fab1510caadff
DakotaLMartinez <dakotaleemusic@gmail.com>
2020-01-07 16:43:01 -0800

    finish devise installation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
 
-  def current_user 
-    User.find_by(name: "Dakota")
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :reviews
   resources :tags
-  resources :movies
+  resources :movies do 
+    resources :reviews
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'movies#index'
 end
